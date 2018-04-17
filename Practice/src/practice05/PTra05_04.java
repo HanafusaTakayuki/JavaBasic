4a4dd0c1fce9aa4ff62ab6a069437c29778f5af6
HanafusaTakayuki <hhtt442200@gmail.com>
2018-04-17 16:59:42 +0900

    PTra05_01〜04 まで完了。 Practice5 完了。

--- Practice/src/practice05/PTra05_04.java
+++ Practice/src/practice05/PTra05_04.java
@@ -22,17 +22,41 @@ public class PTra05_04 {
 		 *
 		 */
 
+		int num = 0;
+
 		// ①
-		System.out.println("0～6の整数を入力してください");
 
-		String line = scanner.nextLine();
+		do {
 
-		switch(line) {
-			case "0":
-				System.out.println("日曜");
-				break;
-			case "1":
-				System.out.println("月曜");
-		}
+			System.out.println("0～6の整数を入力してください");
+
+			String line = scanner.nextLine();
+
+			num = Integer.parseInt(line);
+
+			switch(num) {
+				case 0:
+					System.out.println("日曜");
+					break;
+				case 1:
+					System.out.println("月曜");
+					break;
+				case 2:
+					System.out.println("火曜");
+					break;
+				case 3:
+					System.out.println("水曜");
+					break;
+				case 4:
+					System.out.println("木曜");
+					break;
+				case 5:
+					System.out.println("金曜");
+					break;
+				case 6:
+					System.out.println("土曜");
+			}
+
+		} while(num > 6);
 	}
 }
